d4c11d287a75b6f599c24f972645f175cf4599a9
Lukas Dreier <dreierl@edu.htl-villach.at>
2017-01-28 14:28:54 +0100

    Dynamic map added to StationEditView and AdminMap

--- webApp/uGuide-WebApp/js/adminMap.js
+++ webApp/uGuide-WebApp/js/adminMap.js
@@ -1,7 +1,19 @@
 (function () {
-angular.module('adminMap', [])
+var app = angular.module('adminMap', ['canvasLib']);
 
-.controller('adminMapCtrl', function ($rootScope, $scope, $timeout, stationFactory, ViewConstant) {
+app.directive('backImg', function() {
+    return function(scope, element, attrs) {
+
+        attrs.$observe('backImg',  function(value) {
+            element.css({
+                'background-image': 'url(' + value +')',
+                'background-size' : '100% 100%'
+            });
+        });
+    };
+});
+
+app.controller('adminMapCtrl', function ($rootScope, $scope, $timeout, stationFactory, tdotFactory, canvasLibFactory, ViewConstant) {
     $scope.stations = [];
 
     $scope.alert = {};
@@ -21,6 +33,19 @@ angular.module('adminMap', [])
     $scope.init = function() {
         $scope.jQueryInjection();
 
+        $scope.getStations();
+
+        $scope.getCurrentTdot();
+    }
+
+    $scope.jQueryInjection = function() {
+        //JQuery injection möööö
+        $('modalId').modal('hide');
+        $('body').removeClass('modal-open');
+        $('.modal-backdrop').remove();
+    }
+
+    $scope.getStations = function() {
         stationFactory.getStationsWithPosition().then
         (
             function(successResponse) {
@@ -35,11 +60,92 @@ angular.module('adminMap', [])
         );
     }
 
-    $scope.jQueryInjection = function() {
-        //JQuery injection möööö
-        $('modalId').modal('hide');
-        $('body').removeClass('modal-open');
-        $('.modal-backdrop').remove();
+    $scope.getCurrentTdot = function() {
+        tdotFactory.getCurrentTdot().then
+        (
+            function(successResponse) {
+                $scope.currentTdot = successResponse.data;
+
+                tdotFactory.getMap($scope.currentTdot._id).then
+                (
+                    function(successResponse) {
+                        parseMapFromServer(successResponse.data);
+                    },
+                    function(errorResponse) {
+                        console.log('ERROR -- ' + errorResponse.status + ' ' + errorResponse.data.message);
+                        $scope.addAlert('danger', errorResponse.data.code, "Failed to get map");
+                        $timeout($scope.resetAlert, ViewConstant.timeoutDuration);
+                    }              
+                );
+
+            },
+            function(errorResponse) {
+                console.log('ERROR -- ' + errorResponse.status + ' ' + errorResponse.data.message);
+                $scope.addAlert('danger', errorResponse.data.code, 'Error occured while loading the current TdoT');
+                $timeout($scope.resetAlert, ViewConstant.timeoutDuration);
+            }
+        );
     }
+
+    //MAP - Editor
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+    
+    $scope.currentPoint = undefined;
+    $scope.data = [
+    ];
+
+    function resetHoverPoints() {
+        for (var i = 0; i < $scope.data.length; i++) {
+            $scope.data[i].isHover = false;
+        }
+    }
+
+    function getPoint(x, y) {
+        for (var i = 0; i < $scope.data.length; i++) {
+            if(isPointInCircle(x, y, $scope.data[i].x, $scope.data[i].y, $scope.data[i].amount)) {
+                return $scope.data[i];
+            }
+        }
+    }
+
+    function parseMapFromServer(mapData) {
+        $scope.base64Image = mapData.Map;
+        
+        var points = mapData.Points.map(function(point) {
+            return { tag: point.Tag, x: point.X, y: point.Y, amount: 10, isCurrent: false, isHover: true };
+        });
+
+        $scope.data = points;
+        redraw();
+    }
+
+    function isPointInCircle(x, y, xCircle, yCircle, radius) {
+        return Math.sqrt((x-xCircle)*(x-xCircle) + (y-yCircle)*(y-yCircle)) < radius;
+    }
+
+    function redraw() {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        for (var i = 0; i < $scope.data.length; i++) {
+            if($scope.data[i].isCurrent) {
+                canvasLibFactory.drawDot(context, $scope.data[i], "#FF0000");
+                canvasLibFactory.drawToolTip(context, $scope.data[i]);
+            } else if($scope.data[i].isHover) {
+                canvasLibFactory.drawDot(context, $scope.data[i], "#FFC107");
+                canvasLibFactory.drawToolTip(context, $scope.data[i]);
+            } else {
+                canvasLibFactory.drawDot(context, $scope.data[i], "#ccddff");
+            }
+        }
+     }
+
+    // setup
+    canvas.width = 800;
+    canvas.height = 600;
+    context.globalAlpha = 1.0;
+    context.font = "25px serif";
+    context.beginPath();
+    redraw();   
 });
 })();
